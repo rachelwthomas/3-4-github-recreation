12e4597e920981ab9007c2a5379c9c7071567416
rachelwthomas <58479626+rachelwthomas@users.noreply.github.com>
2020-01-23 17:22:48 -0500

    no progress

--- main.js
+++ main.js
@@ -2,4 +2,32 @@ $(document).ready(function() {
 
 
 
+
+
+
+
+
+//****************************************** organizations section
+
+$.ajax({
+  url: "https://api.github.com/users/rachelwthomas/orgs",
+  Authorization: `token ${GITHUB_API_KEY}`
+  })
+  .done((resp) => {
+    updateOrgs(resp);
+  })
+  .fail((err) => {
+    console.log(err);
+  })
+  .always(() => {
+    // console.log('always fires');
+  });
+
+  let updateOrgs = (orgs) => {
+    console.log(orgs.login)
+  }
+
+
+
+
 });
